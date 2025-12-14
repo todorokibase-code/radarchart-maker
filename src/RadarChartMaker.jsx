a0e37de72de613a0263739a096fa8385853fbb56
todorokibase-code <todorokibase.25@gmail.com>
2025-12-14 15:32:16 +0900

    ツイートボタンを追加

--- src/RadarChartMaker.jsx
+++ src/RadarChartMaker.jsx
@@ -25,23 +25,23 @@ function RadarChartMaker() {
   return (
 
     <>
-      <div>
-        <div style={styles.container}>
-          <div>
-            <Prediction predictionValue={predictionValue} handleChange={setPredictionValue} />
-            <div style={{ backgroundColor: '#f0f0f0', border: 'solid' }}>
-              <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <h2>
-                  勝利者：{predictionValue}
-                </h2>
-              </div>
-
-              <Player player={PLAYERS[0]} pos={0} />
-              <VsComp />
-              <Player player={PLAYERS[1]} pos={1} />
-
+      <div style={styles.container}>
+        <div>
+          <Prediction predictionValue={predictionValue} handleChange={setPredictionValue} />
+          <div style={{ backgroundColor: '#f0f0f0', border: 'solid' }}>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <h2>
+                勝利者：{predictionValue}
+              </h2>
             </div>
+
+            <Player player={PLAYERS[0]} pos={0} />
+            <VsComp />
+            <Player player={PLAYERS[1]} pos={1} />
+
           </div>
+
+          <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="みんなで戦力分析をしよう！" data-hashtags="井上尚弥,リヤド・シーズン" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </div>
       </div>
 
